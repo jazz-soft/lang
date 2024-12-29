248870a6a737b84b92f630c1f3e7c107cd897a07
Sema <jazz-dev@jazz-soft.net>
2024-12-28 23:27:21 -0500

    WIP

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+const {Facet} = require('@codemirror/state');
+const {Language, LanguageSupport, defineLanguageFacet} = require('@codemirror/language');
+
+function Parser() {
+    console.log('Creating parser!');
+}
+Parser.prototype.createParse = function() {
+    console.log('createParse()');
+};
+Parser.prototype.startParse = function() {
+    console.log('startParse()');
+};
+Parser.prototype.parse = function() {
+    console.log('parse()');
+};
+
+const data = 'my language data';
+const myFacet = defineLanguageFacet(data);
+const XP = new Language(myFacet, new Parser(), [], 'langname');
+console.log('MyFacet:', myFacet);
+
+function XPath(conf) {
+    return new LanguageSupport(XP, []);
+};
+
 module.exports = {
+    XPath: XPath
 };
